22757d46afdbd8c754c18e80510a5f412f7be84f
jayjay911 <76834895+jayjay911@users.noreply.github.com>
2024-12-13 11:39:48 +0200

    Update parse.dart

--- lib/src/parser/parse.dart
+++ lib/src/parser/parse.dart
@@ -319,8 +319,7 @@ class Parser {
           final numFmtId = int.parse(node.getAttribute('numFmtId')!);
           final formatCode = node.getAttribute('formatCode')!;
           if (numFmtId < 164) {
-            throw Exception(
-                'custom numFmtId starts at 164 but found a value of $numFmtId');
+            numFmtId += 164;
           }
 
           _excel._numFormats
